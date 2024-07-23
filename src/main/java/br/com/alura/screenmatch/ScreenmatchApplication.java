46143e535a832fb24ba0aece4b9c66e32c14ab09
erickwork <dery.erick@outlook.com>
2024-07-23 01:12:55 -0300

    Implementando o CommandLineRunner

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,10 +1,15 @@
 package br.com.alura.screenmatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner {
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
